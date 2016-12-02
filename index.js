f4504cd73f3b9926f615bfd2fccaaf959698a636
carson hyde <carsonmhyde@gmail.com>
2016-12-01 21:16:14 -0500

    bananas

--- index.js
+++ index.js
@@ -8,23 +8,6 @@ const bcrypt = require('bcryptjs');
 const methodOverride = require('method-override');
 //declare constants
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-
-app.configure(function () {
- app.use(allowCrossDomain)
-});
 
 
 app.engine('html', mustacheExpress());
@@ -34,7 +17,6 @@ app.use("/", express.static(__dirname + '/public'));
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 //call functions
 //when pushing a project, module file ignored
 //npm install will install dependencies
